d5b7058c3322ac6484e110e9d590aff59eab7339
Ashutosh Gola <ashutoshgola91@gmail.com>
2026-05-02 01:39:31 +0530

    added backend url

--- frontend/src/api/axios.ts
+++ frontend/src/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:8000/api"
+  baseURL: "https://team-task-management-production-d70f.up.railway.app/"
 });
 
 API.interceptors.request.use((req) => {
